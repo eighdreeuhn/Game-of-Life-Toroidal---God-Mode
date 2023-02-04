7e1a5467ea1c2312b600200f6b4a8079e3e6bdcf
Adrian Gioia <afgioia@gmail.com>
2023-02-04 14:30:14 -0800

    Slowed time by three.

--- sketch.js
+++ sketch.js
@@ -5,57 +5,61 @@ const reverb = new Tone.Freeverb({
   dampening: 1000
 }).toDestination()
 const voice = new Tone.MonoSynth({
-    envelope: {
-        attack: 0.5,
-        decay: 1,
-        sustain: 1
-    },
-    oscillator:{
-        type: 'fatsine'
-    },
-    volume: -6,
-    portamento: 0.5
+  envelope: {
+    attack: 0.5,
+    decay: 1,
+    sustain: 1
+  },
+  oscillator: {
+    type: 'fatsine'
+  },
+  volume: -6,
+  portamento: 0.5
 }).toDestination()
 const hihat = new Tone.MetalSynth({
-    volume: -20
+  volume: -20
 }).toDestination()
-const loop = new Tone.Loop((time) => {
-    hihat.triggerAttackRelease('A6', '+2n')
-}, "2m").start(0)
+const loop = new Tone.Loop(time => {
+  hihat.triggerAttackRelease('A6', '+2n')
+}, '2m').start(0)
 const neighborhood = [
-    [-1, -1],
-    [-1, 0],
-    [-1, 1],
-    [0, -1],
-    [0, 1],
-    [1, -1],
-    [1, 0],
-    [1, 1]
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, -1],
+  [0, 1],
+  [1, -1],
+  [1, 0],
+  [1, 1]
 ]
 let world = []
+let time = 0
 
 function setup () {
   Tone.Transport.start()
   createCanvas(1200, 600)
   world = []
-  for (let i = 0; i < 120; i++) world.push([...Array(240)].map(_ => ~~(Math.random() * 2)))
+  for (let i = 0; i < 120; i++)
+    world.push([...Array(240)].map(_ => ~~(Math.random() * 2)))
 }
 
 function draw () {
-  const newWorld = [...world.map(row => [...row])]
-  for (let i = 0; i < 240; i++) {
-    for (let j = 0; j < 120; j++) {
-      const hood = neighbors(j, i)
-      if (hood < 2 || hood > 3) {
-        newWorld[j][i] = 0
-      } else if (hood === 3) {
-        newWorld[j][i] = 1
+  if (time % 3 === 0) {
+    const newWorld = [...world.map(row => [...row])]
+    for (let i = 0; i < 240; i++) {
+      for (let j = 0; j < 120; j++) {
+        const hood = neighbors(j, i)
+        if (hood < 2 || hood > 3) {
+          newWorld[j][i] = 0
+        } else if (hood === 3) {
+          newWorld[j][i] = 1
+        }
+        newWorld[j][i] ? fill(190, 0, 0) : fill(160)
+        square(5 * i, 5 * j, 4)
       }
-      newWorld[j][i] ? fill(190, 0, 0) : fill(160)
-      square(5 * i, 5 * j, 4)
     }
+    world = [...newWorld.map(row => [...row])]
   }
-  world = [...newWorld.map(row => [...row])]
 }
 
 function neighbors (r, c) {
@@ -63,7 +67,9 @@ function neighbors (r, c) {
     const [cr, cc] = [r + dy, c + dx]
     return (
       n +
-      world[cr >= 120 ? 0 : cr < 0 ? 119 : cr][cc >= 240 ? 0 : cc < 0 ? 239 : cc]
+      world[cr >= 120 ? 0 : cr < 0 ? 119 : cr][
+        cc >= 240 ? 0 : cc < 0 ? 239 : cc
+      ]
     )
   }, 0)
 }
