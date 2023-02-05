f7680453aa9e2a77893d3af4622f8968cf88f7c6
Adrian Gioia <afgioia@gmail.com>
2023-02-04 17:26:17 -0800

    Color adjustment. Might not stay.

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ const reverb = new Tone.Freeverb({
   roomSize: 0.5,
   dampening: 1000
 }).toDestination()
+
 const voice = new Tone.MonoSynth({
   envelope: {
     attack: 0.5,
@@ -16,12 +17,13 @@ const voice = new Tone.MonoSynth({
   volume: -6,
   portamento: 0.5
 }).toDestination()
-const hihat = new Tone.MetalSynth({
-  volume: -20
-}).toDestination()
+
+const hihat = new Tone.MetalSynth({ volume: -20 }).toDestination()
+
 const loop = new Tone.Loop(time => {
   hihat.triggerAttackRelease('A6', '+2n')
 }, '2m').start(0)
+
 const neighborhood = [
   [-1, -1],
   [-1, 0],
@@ -33,33 +35,32 @@ const neighborhood = [
   [1, 1]
 ]
 let world = []
-let time = 0
 
 function setup () {
   Tone.Transport.start()
   createCanvas(1200, 600)
+  noStroke()
+  frameRate(30)
   world = []
   for (let i = 0; i < 120; i++)
     world.push([...Array(240)].map(_ => ~~(Math.random() * 2)))
 }
 
 function draw () {
-  if (time % 3 === 0) {
-    const newWorld = [...world.map(row => [...row])]
-    for (let i = 0; i < 240; i++) {
-      for (let j = 0; j < 120; j++) {
-        const hood = neighbors(j, i)
-        if (hood < 2 || hood > 3) {
-          newWorld[j][i] = 0
-        } else if (hood === 3) {
-          newWorld[j][i] = 1
-        }
-        newWorld[j][i] ? fill(190, 0, 0) : fill(160)
-        square(5 * i, 5 * j, 4)
+  const newWorld = [...world.map(row => [...row])]
+  for (let i = 0; i < 240; i++) {
+    for (let j = 0; j < 120; j++) {
+      const hood = neighbors(j, i)
+      if (hood < 2 || hood > 3) {
+        newWorld[j][i] = 0
+      } else if (hood === 3) {
+        newWorld[j][i] = 1
       }
+      newWorld[j][i] ? fill(190, 0, 0) : fill(0, 130, 75)
+      square(5 * i, 5 * j, 5)
     }
-    world = [...newWorld.map(row => [...row])]
   }
+  world = [...newWorld.map(row => [...row])]
 }
 
 function neighbors (r, c) {
