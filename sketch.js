9906d14e6cf49309ada45b281b4a030e043bed74
Adrian Gioia <afgioia@gmail.com>
2023-04-07 18:02:07 -0700

    Revert to random seeding

--- sketch.js
+++ sketch.js
@@ -45,8 +45,8 @@ function setup () {
   noStroke()
   world = []
   for (let i = 0; i < 120; i++)
-    world.push(Array(240).fill(0))
-    // world.push([...Array(240)].map(_ => Math.floor(Math.random() * 2)))
+    // world.push([...Array(240)].map(_ => [1,0][~~(Math.random()*2)]))
+    world.push([...Array(240)].map(_ => Math.floor(Math.random() * 2)))
 }
 
 function draw () {
